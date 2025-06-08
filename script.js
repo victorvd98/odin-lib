2f42f2c11084bee73bb8ba83b2a3f9c189dd2a09
victorvd98 <victor.van.damme@icloud.com>
2025-06-08 15:18:06 +0200

    wtv yeah we coded this biiiitch

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const myLibrary = [];
 
-function Book(title, author) { //object constructor
-    this.title = title;
-    this.author = author;
-    this.id = crypto.randomUUID();
-    this.read = false;
-}
-
-function listLibrary() { //function to list all books in my library
+function listLibrary() { //function to list all books in my library in HTML
 
     if (myLibrary.length > 0) {
 
         for (let i = 0; i < myLibrary.length; i++) {
             const li = document.createElement('li');
-            li.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}`;
+            li.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}`; //template literals
             document.getElementById("book-list").appendChild(li);
         }
     } else {
         console.log("Library is empty. Please add some books.");
     }
+}
+
+function Book(title, author) { //object constructor
+    this.title = title;
+    this.author = author;
+    this.id = crypto.randomUUID();
+    this.read = false;
 }
 
 function addBookToLibrary(title, author) {  //function to add books to my library
@@ -27,5 +27,12 @@ function addBookToLibrary(title, author) {  //function to add books to my librar
     listLibrary();
 }
 
-const form = document.getElementById('book-form');
+const form = document.getElementById('add-book');
+
+form.addEventListener('submit', e => {
+    e.preventDefault(); //prevent page from refreshing / other default actions by browser
+    const formTitle = document.getElementById('title').value;
+    const formAuthor = document.getElementById('author').value;
+    addBookToLibrary(formTitle, formAuthor);
+});
 
